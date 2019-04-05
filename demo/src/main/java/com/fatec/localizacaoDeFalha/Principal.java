73f91a93444aaaaee87765e1b5e269bde7e915b4
edson <edson@10.70.11.181>
2019-04-05 17:53:13 -0300

    Versão para experimentação

---
+++ demo/src/main/java/com/fatec/localizacaoDeFalha/Principal.java
@@ -0,0 +1,10 @@
+package com.fatec.localizacaoDeFalha;
+
+public class Principal {
+	public static void main (String args[]) {
+		Mediana m = new Mediana();
+		System.out.println(m.determinaMediana(2, 1, 3));
+		System.out.println(m.determinaMediana(3, 2, 1));
+	}
+
+}
